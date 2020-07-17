7ef5cf5c220954196308dd78a1b7ea9c0588f666
naeemsifat <sifat.ete@pust.ac.bd>
2020-07-17 13:13:35 +0600

    remove woodCalculator.js

--- woodCalculator.js
+++
@@ -1,8 +0,0 @@
-function woodCalculator(chair,table,bed)//Function starts from here
-{
-    let totalWood;
-    totalWood=(chair*1)+(table*3)+(bed*5);//Total wood is calculated in this line
-    return totalWood;
-}
-const result=woodCalculator(2,5,3);
-console.log("Total",result,"cubic feet wood needed");//Display the total wood
